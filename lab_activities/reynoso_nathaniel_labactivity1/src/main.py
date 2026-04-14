512544dec10362372a9ff132e079e40925f8a42c
NathanielReynoso <61068192+NathanielReynoso@users.noreply.github.com>
2026-04-14 20:47:39 +0800

    Add files via upload

--- lab_activities/reynoso_nathaniel_labactivity1/src/main.py
+++ lab_activities/reynoso_nathaniel_labactivity1/src/main.py
@@ -1,1 +1,23 @@
+# -----------------------------------------------------------------
+# File in src/main.py
+# Main file wherein three functions were made for three test cases.
+# Add, Subtract, and Multiply function were used to test.
+# -----------------------------------------------------------------
 
+
+def add(a, b):
+    return a + b
+"""Adds two numbers"""
+
+def subtract(a, b):
+    return a - b
+"""Subtracts two numbers"""
+
+def multiply(a, b):
+    return a * b
+"""Multiplies two numbers"""
+
+# Workaround for using onecompiler to run tests.py
+if __name__ == '__main__':
+    import os
+    os.system("python3 tests.py")
